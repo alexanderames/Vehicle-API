cb30862a749b0dd0a26095142f08a22b2f22b566
alexander ames <alexanderthomasames@gmail.com>
2018-07-01 13:49:10 -0700

    create validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,5 @@
 class Vehicle < ApplicationRecord
+  validates :vin, presence: true, uniqueness: true
+  validates :mileage, presence: true
+  validates :plate_state, presence: true
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should test that the factory is valid' do
+      expect(build :vehicle).to be_valid
+    end
+
+    it 'should validate the presence of vin' do
+      v = build :vehicle, vin: ''
+      expect(v).not_to be_valid
+      expect(v.errors.messages[:vin]).to include("can't be blank")
+    end
+
+    it 'should validate the uniqueness of vin' do
+      v = create :vehicle
+      invalid_vehicle = build :vehicle, vin: v.vin
+      expect(invalid_vehicle).not_to be_valid
+    end
+
+    it 'should validate the presence of mileage' do
+      v = build :vehicle, mileage: ''
+      expect(v).not_to be_valid
+      expect(v.errors.messages[:mileage]).to include("can't be blank")
+    end
+
+    it 'should validate the presence of plate_state' do
+      v = build :vehicle, plate_state: ''
+      expect(v).not_to be_valid
+      expect(v.errors.messages[:plate_state]).to include("can't be blank")
+    end
+  end
 end
