47929c6b5a0d9185ff67e1ed653d21c2e9e21ab9
alexander ames <alexanderthomasames@gmail.com>
2018-07-08 10:45:12 -0700

    add user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701200159) do
+ActiveRecord::Schema.define(version: 20180707202119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "name"
+    t.string "url"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.string "vin"
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    login "MyString"
+    name "MyString"
+    url "MyString"
+    avatar_url "MyString"
+  end
+end
